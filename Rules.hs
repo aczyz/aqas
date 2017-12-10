f5b7f5f1b4ca1e9a5ac127c80aed729fa28821d2
atomczyk <33434858+atomczyk@users.noreply.github.com>
2017-12-10 21:18:44 +0100

    Update Rules.hs

--- Rules.hs
+++ Rules.hs
@@ -3,80 +3,88 @@ module Rules where
 import Sequents
 import Formulas
 
---lAlpha :: Sequent -> HyperSequent  --left alpha rule
+-- fst formulas's ingredients in a list (left side)   (why?)
+lAlpha :: Sequent -> [Form]  
 lAlpha (x:xs, ys) = alphaIng x
 
-rAlpha :: Sequent -> (Form, Form)  --right alpha rule
+rAlpha :: Sequent -> [Form]  
 rAlpha (xs, y:ys) = alphaIng y
 
-lBeta :: Sequent -> (Form, Form) -- left beta rule
+lBeta :: Sequent -> [Form] -- left beta rule
 lBeta (x:xs, ys) = betaIng x
 
-rBeta :: Sequent -> (Form, Form) --right beta rule
+rBeta :: Sequent -> [Form]
 rBeta (xs, y:ys) = betaIng y
 
-lDNeg :: Sequent -> HyperSequent --left double negation rule
+lDNeg :: Sequent -> HyperSequent -- not necessary as dbl neg is already an alpha, keeping just in case
 lDNeg (x:xs, ys) = case x of 
                         N a -> case a of
                                     N b -> [(b:xs, ys)]
                                     _ -> [(x:xs, ys)]
                         _ -> lDNeg (xs ++ [x], ys)
 
-rDNeg :: Sequent -> HyperSequent --right double negation rule
+rDNeg :: Sequent -> HyperSequent 
 rDNeg (xs, y:ys) = case y of 
                         N a -> case a of
                                     N b -> [(xs, b:ys)]
                                     _ -> [(xs, y:ys)]
                         _ -> rDNeg (xs, ys ++ [y])
 
-sort' :: Sequent -> Sequent                        
-sort' (x:xs, y:ys) = (z ++ [x] ++ v, v' ++ [y] ++ z')
+-- sorting list ([alpha, beta], [beta, alpha])
+sort' :: Sequent -> Sequent
+sort' (x, y) = (z ++ v, v' ++ z')
             where
-                z = filter alphaForm xs
-                v = filter betaForm xs
-                z' = filter alphaForm ys
-                v' = filter betaForm ys
+                z = filter alphaForm x
+                v = filter betaForm x
+                z' = filter alphaForm y
+                v' = filter betaForm y
 
+-- another way of doing so
 sort'' :: Sequent -> Sequent                
 sort'' x = (lAlphaSeq x ++ lBetaSeq x, rBetaSeq x ++ rAlphaSeq x)
 
-insert :: Form -> Sequent -> [Sequent]  --tylko sekwent i jedna formuła (plus podwójna negacja)
-insert a (x, y)
-                | alphaForm a = [(a:x, y)]
-                | betaForm a = [(z ++ [a] ++ v, y)]
-                    where
-                        z = lAlphaSeq (x, y)
-                        v = lBetaSeq (x, y) 
+-- insert formula on the left side
+insertL :: Form -> Sequent -> Sequent
+insertL a (x, y)
+                | alphaForm a = (a:x, y)
+                | betaForm a = (x ++ [a], y)
 
-alphaIng :: Form -> (Form, Form)   --[Form] (?)
+-- insert formula, right side
+insertR :: Form -> Sequent -> Sequent
+insertR a (x, y)
+                | alphaForm a = (x, a:y)
+                | betaForm a = (x, y ++ [a])
+
+-- list of alpha formula's ingredients
+alphaIng :: Form -> [Form]   --[Form] (?)
 alphaIng x = case x of
-                C z y -> (z, y)
+                C z y -> [z, y]
                 N v -> case v of 
-                            D z y -> (N z, N y)
-                            I z y -> (z, N y)
-
-betaIng :: Form -> (Form, Form)
-betaIng x = case x of
-                I a b -> (N a, b)
-                D a b -> (a, b)
-                N v -> case v of
-                            C a b -> (N a, N b)
-
-
-
+                            D z y -> [N z, N y]
+                            I z y -> [z, N y]
+                            N z -> [z]
                             
-{-
+-- same but for beta
+betaIng :: Form -> [Form]
+betaIng x = case x of
+                I a b -> [N a, b]
+                D a b -> [a, b]
+                N v -> case v of
+                            C a b -> [N a, N b]
 
- sort([Alpha1, Alpha2, ... Beta] [Beta1, Beta2, ... Alpha])
- alpha ingredients
- take first formula and put its products/ingredients in correct place (sorted), depending on it being alpha or beta
- and again
+-- this one will check first formula and insert it
+rule :: Sequent -> [Sequent]
+rule (x:xs, y:ys)
+            | alphaForm x = case (alphaIng x) of
+                                [z, v] -> [(x:xs, y:ys), insertL z (insertL v (xs, y:ys))]
+                                [z] -> [(x:xs, y:ys), insertL z (xs, y:ys)]
+            | betaForm y = case (betaIng y) of
+                                [z, v] -> [(x:xs, y:ys), insertR z (insertL v (x:xs, ys))]
+                                [z] -> [(x:xs, y:ys), insertR z (x:xs, ys)]
 
- alpha/beta ingredients (table) :: Formula -> [Formula]
- 
 
-mRule (xs, ys)   
-            | alphaSeq (xs, ys) = (lAlpha (xs, ys)) <*> (rAlpha (xs, ys))
-            -}
+-- examples 
+ex = sort' ([D (V 1) (V 2), C (V 4) (V 5), N (V 1)], [V 1, V 2, D (V 1) (V 2)])
+test = rule ex
 
 
