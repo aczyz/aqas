0af5634ec2e7bf00fc7e89897328a32202b6e500
atomczyk <33434858+atomczyk@users.noreply.github.com>
2018-01-11 18:39:13 +0100

    Update Rules.hs

--- Rules.hs
+++ Rules.hs
@@ -32,10 +32,8 @@ r_lit (x, y) = filter isLiteral y
 -- added variables, the reason is stated below 
 alphaIng :: Form -> [Form]
 alphaIng x = case x of
-                V _ -> [x]
                 C z y -> [z, y]
                 N v -> case v of
-                            V _ -> [x]
                             D z y -> [N z, N y]
                             I z y -> [z, N y]
                             N z -> [z]
@@ -44,43 +42,24 @@ alphaIng x = case x of
 -- added variables as there were some problems when trying out one of the examples below (test2)
 betaIng :: Form -> [Form]
 betaIng x = case x of
-                V _ -> [x]
                 I a b -> [N a, b]
                 D a b -> [a, b]
                 N v -> case v of
-                            V _ -> [x]
                             C a b -> [N a, N b]
 
-
--- sorting list into ([alpha, beta] [beta, alpha])
-sort' :: Sequent -> Sequent
-sort' (x, y) = (z ++ v, v' ++ z')
-            where
-                z = filter alphaForm x
-                v = filter betaForm x
-                z' = filter alphaForm y
-                v' = filter betaForm y
-
--- another way of doing so
-sort'' :: Sequent -> Sequent
-sort'' x = (lAlpha x ++ lBeta x, rBeta x ++ rAlpha x)
-
 -- changing sequent type into tuple
-seq_analys :: Sequent -> ([Form], [Form], [Form], [Form], [Form], [Form])
+seq_analys :: Sequent -> HSeqSorted
 seq_analys x = (lAlpha x, lBeta x, l_lit x, rBeta x, rAlpha x, r_lit x)
 
--- example to show that it works
-test1 = seq_analys ([C (V 1) (V 2), V 4, V 6, D (V 3) (V 4)], [C (V 1) (V 3), V 4, V 8])
-
 -- insert formula in the antecedent of a sequent
-insert_ant :: Form -> ([Form], [Form], [Form], [Form], [Form], [Form]) -> ([Form], [Form], [Form], [Form], [Form], [Form])
+insert_ant :: Form -> HSeqSorted -> HSeqSorted
 insert_ant y (x, a, b, c, d, e)
                 | alphaForm y = (y:x, a, b, c, d, e)
                 | betaForm y = (x, y:a, b, c, d, e)
                 | isLiteral y = (x, a, y:b, c, d, e)
                 
 -- insert formula in a succedent of a sequent
-insert_succ :: Form -> ([Form], [Form], [Form], [Form], [Form], [Form]) -> ([Form], [Form], [Form], [Form], [Form], [Form])
+insert_succ :: Form -> HSeqSorted -> HSeqSorted
 insert_succ y (x, a, b, c, d, e)
                 | betaForm y = (x, a, b, y:c, d, e)
                 | alphaForm y = (x, a, b, c, y:d, e)
@@ -89,28 +68,25 @@ insert_succ y (x, a, b, c, d, e)
 -- inserting components of given formula (unctions below will vary in regards to formula's type and its side)
 -- in the correct place, making that into a list (as in the case of alpha formula on the right side and beta
 -- on the left we'll get two sequents), to make it easier to get those back into sequent form
-l_ins_alpha :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> [([Form], [Form], [Form], [Form], [Form], [Form])]
+l_ins_alpha :: HSeqSorted -> [HSeqSorted]
 l_ins_alpha (x:xs, a, b, c, d, e)
                         | alphaForm x = case (alphaIng x) of
                                                            [z, v] -> [insert_ant z (insert_ant v (xs, a, b, c, d, e))]
                                                            [z] -> [insert_ant z (xs, a, b, c, d, e)]
 
-r_ins_alpha :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> [([Form], [Form], [Form], [Form], [Form], [Form])]
+r_ins_alpha :: HSeqSorted -> [HSeqSorted]
 r_ins_alpha (x, a, b, c, d:ds, e)
                         | alphaForm d  = case (alphaIng d) of
                                                            [z, v] -> [insert_succ z (x, a, b, c, ds, e), insert_succ v (x, a, b, c, ds, e)]
                                                            [z] -> [insert_succ z (x, a, b, c, ds, e)]
-                        | isLiteral d = [(x, a, b, c, ds, d:e)] -- added to check something
 
-l_ins_beta :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> [([Form], [Form], [Form], [Form], [Form], [Form])]
+l_ins_beta :: HSeqSorted -> [HSeqSorted]
 l_ins_beta (x, a:as, b, c, d, e)
                         | betaForm a = case (betaIng a) of
                                                            [z, v] -> [insert_ant z (x, as, b, c, d, e), insert_ant v (x, as, b, c, d, e)]
                                                            [z] -> [insert_ant z (x, as, b, c, d, e)]
-                        | isLiteral a = [(x, as, a:b, c, d, e)] --same, tbh it won't be needed in future
-
-
-r_ins_beta :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> [([Form], [Form], [Form], [Form], [Form], [Form])]
+                
+r_ins_beta :: HSeqSorted -> [HSeqSorted]
 r_ins_beta (x, a, b, c:cs, d, e)
                         | betaForm c = case (betaIng c) of
                                                            [z, v] -> [insert_succ z (insert_succ v (x, a, b, cs, d, e))]
@@ -118,7 +94,7 @@ r_ins_beta (x, a, b, c:cs, d, e)
 
 
 -- applying proper rules to non-empty sets
-apply_rule :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> [([Form], [Form], [Form], [Form], [Form], [Form])]
+apply_rule :: HSeqSorted -> [HSeqSorted]
 apply_rule (z, c, v, a, b, d)
               | not (null z) = l_ins_alpha (z, c, v, a, b, d)
               | not (null c) = l_ins_beta (z, c, v, a, b, d)
@@ -127,7 +103,7 @@ apply_rule (z, c, v, a, b, d)
 
 
 -- getting back into sequent form
-seq_creator :: ([Form], [Form], [Form], [Form], [Form], [Form]) -> Sequent
+seq_creator :: HSeqSorted -> Sequent
 seq_creator (x, y, z, a, b, c) = (x ++ y ++ z, a ++ b ++ c)
 
 
@@ -143,15 +119,15 @@ create_hseq [] = []
 create_hseq (x:xs) = case isAtomic x of
                           True -> x : create_hseq xs
                           False -> hyper_creator (x:xs)
+                          
+drv :: [HyperSequent] -> [HyperSequent]
+drv (x:xs) = [until (isMinimal) create_hseq x] ++ (x:xs)                          
 
+---------------------------------------------------------
+
+test1 = seq_analys ([C (V 1) (V 2), V 4, V 6, D (V 3) (V 4)], [C (V 1) (V 3), V 4, V 8])
 test3 = create_hseq [([I (V 1) (I (V 2) (V 3)), C (V 2) (V 4)], [I (V 2) (V 3)])]
 test31 = seq_analys (head [([V 2,V 4,I (V 2) (V 3),I (V 1) (I (V 2) (V 3))],[])])
-
-
-drv :: [HyperSequent] -> [HyperSequent]
-drv (x:xs) = [until (isMinimal) create_hseq x] ++ (x:xs)
-
-
 test2 = drv [[([I (V 1) (I (V 2) (V 3)), C (V 2) (V 4)], [I (V 2) (V 3)])]]  --success
 test4 = drv [[([C (V 1) (V 2)], [V 3])]]  --success 
 test6 = drv [[([C (I (N (V 1)) (D (V 2) (V 1))) (D (V 1) (V 2))], [D (V 1) (N (V 1)), I (V 10) (V 2), C (V 5) (V 6), C (V 5) (V 7), C (V 5) (V 22), C (V 5) (V 44)])]] --success
