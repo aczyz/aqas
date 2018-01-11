be4d115dbba7e49b6ad4e5e967a5fcb83ddd9c5e
kingaord <33434900+kingaord@users.noreply.github.com>
2018-01-11 15:10:45 +0100

    Small changes

--- Sequents.hs
+++ Sequents.hs
@@ -8,64 +8,47 @@ type HyperSequent = [Sequent]
 lBetaSeq :: Sequent -> [Form]                         --creates list with betas (left side)
 lBetaSeq ([], _) = []
 lBetaSeq (x:xs, ys)
-                    | betaForm x = x: lBetaSeq (xs, ys)
+                    | betaForm x = x : lBetaSeq (xs, ys)
                     | otherwise  = lBetaSeq (xs, ys)
 
 lAlphaSeq :: Sequent -> [Form]                        --creates list with alphas (left side)
 lAlphaSeq ([], _) = []
 lAlphaSeq (x:xs, ys)
-                    | alphaForm x = x: lAlphaSeq (xs, ys)
-                    | otherwise  = lAlphaSeq (xs, ys)
+                    | alphaForm x = x : lAlphaSeq (xs, ys)
+                    | otherwise   = lAlphaSeq (xs, ys)
 
 rBetaSeq :: Sequent -> [Form]                         --creates list with betas (right side)
 rBetaSeq (_, []) = []
 rBetaSeq (xs, y:ys)
-                  | betaForm y = y : lAlphaSeq (xs, ys)
-                  | otherwise  = rBetaSeq (xs, ys)
+                    | betaForm y = y : rBetaSeq (xs, ys)
+                    | otherwise  = rBetaSeq (xs, ys)
 
 rAlphaSeq :: Sequent -> [Form]                        --creates list with alphas (right side)
 rAlphaSeq (_, []) = []
 rAlphaSeq (xs, y:ys)
                     | alphaForm y = y : rAlphaSeq (xs, ys)
-                    | otherwise  = rAlphaSeq (xs, ys)
+                    | otherwise   = rAlphaSeq (xs, ys)
 
--- traktujemy dNeg jako alpha, więc nie jest potrzebne
-{-
-lDNegSeq :: Sequent -> Bool                         --if there are any double negations on the left
-lDNegSeq ([], _) = False
-lDNegSeq (x:xs, ys) 
-                    | dNegForm x = True
-                    | otherwise  = lDNegSeq (xs, ys)
-
-rDNegSeq :: Sequent -> Bool                         --if there are any double negations on the right
-rDNegSeq (_, []) = False
-rDNegSeq (xs, y:ys)
-                    | dNegForm y = True
-                    | otherwise  = rDNegSeq (xs, ys)
--}
-
--- zobaczyć funkcję 'all'
 isAtomic :: Sequent -> Bool                         --if sequent is atomic
 isAtomic (x, y) = all isLiteral x && all isLiteral y
 
--- zobaczyć funkcję 'all'
 isMinimal :: HyperSequent -> Bool                   --if Hypersequent is minimal
-isMinimal (x:xs) = all isAtomic (x:xs) 
+isMinimal xs = all isAtomic xs 
 
 isClosed :: Sequent -> Bool                         --if Sequent is closed
 isClosed ([], [])     = False
 isClosed ([], y:ys)   = case y of
                           N a -> if elem a (y:ys) then True else isClosed ([], ys)
                           _   -> if elem (N y) ys then True else isClosed ([], ys)
-isClosed (x:xs, ys) = case x of
+isClosed (x:xs, ys)   = case x of
                           N b -> if elem b xs || elem (N b) ys then True else isClosed (xs, ys)
                           _   -> if elem x ys || elem (N x) xs then True else isClosed (xs, ys)
 
 
                          
 abdProblem :: HyperSequent -> Bool                  --if Hypersequent is an abductive problem
-abdProblem []     = False
-abdProblem (x:xs) = all isClosed xs 
+abdProblem [] = False
+abdProblem xs = all isClosed xs 
 
 atomAbdProblem :: HyperSequent -> Bool              --if Hypersequent is an atomic abductive problem
 atomAbdProblem xs = if abdProblem xs && isMinimal xs then True else False
