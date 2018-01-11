67ed52c6163ca7a6685fdb7607a80a09b7fc92b1
kingaord <33434900+kingaord@users.noreply.github.com>
2018-01-11 15:08:13 +0100

    Small change in betaForm

--- Formulas.hs
+++ Formulas.hs
@@ -13,26 +13,18 @@ isLiteral x = case x of
                 V _ -> True
                 N z -> case z of
                             V _ -> True
-                            _ -> False
+                            _   -> False
                 _   -> False
 
 alphaForm :: Form -> Bool   --check if alpha
 alphaForm x = case x of 
-                    V _   -> True    -- ??
                     C _ _ -> True
-                    N z -> case z of 
-                            D _ _ -> True
-                            I _ _ -> True 
-                            N z -> True
-                            _ -> False
-                    _ -> False        
+                    N z   -> case z of 
+                                D _ _ -> True
+                                I _ _ -> True 
+                                N _   -> True
+                                _     -> False
+                    _     -> False        
 
 betaForm :: Form -> Bool   -- check if beta
-betaForm x = not (alphaForm x) -- not alpha                            
-
-{- dNegForm :: Form -> Bool   --check if double negation
-dNegForm x = case x of 
-                  N a -> case a of
-                              N _ -> True
-                              _ -> False
-                  _ -> False -}
+betaForm x = not (alphaForm x) && not (isLiteral x) -- not alpha
