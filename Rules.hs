657261775aef1d9464fba6f825bb6e652bd10e43
atomczyk <33434858+atomczyk@users.noreply.github.com>
2018-07-12 16:24:55 +0200

    Update Rules.hs

--- Rules.hs
+++ Rules.hs
@@ -147,3 +147,157 @@ rmv (x:xs) = case incnst x of
                     _ -> x : rmv xs
 
 test0 = rmv (hs_list test_set)
+
+
+------------------------------------------------------------------
+-- restriction rules and other functions
+
+-- creating list of hintikka sets which do not
+-- consist of negation of a given formula
+-- (for 1st abd rule)
+{-r1_abd :: [[Form]] -> Form -> [[Form]
+r1_abd [] y  = []
+r1_abd (x:xs) y | elem y x     = r1_abd xs ys
+                | otherwise    = y : r1_abd xs y 
+
+--foldr :: (a -> b -> b)  -> b -> [a] -> b
+{-
+ -
+ - ([Form], [[Form]]) -> ([Form], [[Form]])
+r1_abd ([], xs]    = ([], xs)
+r1_abd [(y:ys),(x:xs)]  | elem y x   = r1_abd y xs
+                        | otherwise  = ...
+
+f0 :: Form -> [HSet] -> [HSet]
+f0 y []  = []
+f0 y (x:xs)  | elem y x = f0 y xs
+             | otherwise    = x : f0 y xs 
+
+
+f1 :: Form -> ([Form], [HSet]) -> ([Form], [Hset])
+f1 y ([], (x:xs)] | y `elem` x = if
+                  |  
+
+f2 :: [Seq] -> [Form]
+f2 [] = []
+f2 x:xs = ant1 x ++ f2 xs
+ 
+ -}
+
+-- creating list with hintikka sets which 
+-- do not consist of a given formula
+-- (for 2nd abd rule, antecedent of implication)
+r2_abd :: [[Form]] -> Form -> [[Form]]
+r2_abd [] y = []
+r2_abd (x:xs) y | elem y x  = r2_abd xs y
+                | otherwise = x : r2_abd xs y
+
+
+-- creating list of hintikka sets which 
+-- more general, checking every formula 
+-- from the derivation 
+apply_r1 :: [Form] -> [[Form]] -> [[Form]]
+apply_r1 [] zs     = []
+apply_r1 (x:xs) zs = r1_abd zs x ++ apply_r1 xs zs 
+
+--ap_r1 :: (a -> b -> b) -> [a] -> b -> b
+--ap_r1 f x y = foldr (\z s acc -> (++) (f z s)  acc) []
+
+-- same as above, but checking 2nd abd rule
+-- on list with literals from derivation
+apply_r2 :: [Form] -> [[Form]] -> [[Form]]
+apply_r2 [] zs     = []
+apply_r2 (x:xs) zs = r2_abd zs x ++ apply_r2 xs zs  
+
+-- using the above function, checking the whole
+-- hypersequent (succedent of it)
+-- and creating list with hintikka sets 
+-- which do not consists of a negation of a given formula
+f3' :: HyperSequent -> [[Form]] -> [[Form]]
+f3' [] zs     = []
+f3' (x:xs) zs = apply_r1 (sc1 x) zs ++ f3' xs zs 
+
+
+-- similar but with 2nd abd rule
+f4' :: HyperSequent -> [[Form]] -> [[Form]]
+f4' [] zs     = []
+f4' (x:xs) zs = apply_r2 (ant1 x) zs ++ f4' xs zs
+
+-- succedent of a sequent
+sc1 :: Sequent -> [Form]
+sc1 (x, y) = x
+
+-- antecedent of a sequent
+ant1 :: Sequent -> [Form]
+ant1 (x, y) =  y
+
+
+-- adding literals to separate list
+f5' :: [Form] -> [Form] -> [Form]
+f5' [] ys     = []
+f5' (x:xs) ys = if elem (N x) ys then f5' xs ys else x: f5' xs ys
+
+f51 ::[Form] -> [[Form]] -> [Form]
+f51 xs [] = []
+f51 xs (y:ys) = f5' xs y ++ f51 xs ys
+-}
+{-
+adding literals not appearing in the hintikka set to separate list
+f6 :: HyperSequent -> [[Form]] -> ([[Form]], [Form])
+f6 (x:xs) zs = (f3 ( ...
+-}
+
+{-
+f0 ::  Form -> [[Form]] -> [[Form]]   --zbiór hintikka setów,  w których formuła nie występuje 
+
+
+f1 ::  Form -> ([Form], [[Form]]) -> ([Form], [[Form]])
+
+
+f2 : [[Form]] -> ([Form], [[Form]]) -- [A1, A2, T, ..., An ...] T = te, których nie można zamknąć 
+--generalizacja dla hipersewkentu
+
+
+--jesli f2 zwraca (_, []) to 
+-- decyzja czy przechodzimy do drugiego literału
+f3 :: [Sequent] -> [Form] 
+
+-}
+
+--jeżeli literał znajduje sie w secie to zwraca True
+--w przeciwnym wypadku False
+fspr :: Form -> [Form] -> Bool 
+fspr  y xs | elem y xs  = True
+           | otherwise = False
+
+f0 :: Form -> [[Form]] -> [[Form]]
+f0 x [] = []
+f0 x (y:ys) | fspr x y  = f0 x ys
+            | otherwise = (y : f0 x ys)
+
+-- dodaje do pierwszego elementu pary negacje sprawdzanego literału 
+f1 :: Form -> ([Form], [[Form]]) -> ([Form], [[Form]])
+f1 x (zs, (y:ys)) | fspr x y  = f1 x ([], ys)
+                  | otherwise = (((N x) : zs), [y])
+
+--f2 :: [[Form]] -> ([Form], [[Form]])
+
+
+-- succedent of a sequent
+sc1 :: Sequent -> [Form]
+sc1 (x, y) = y
+
+-- antecedent of a sequent
+ant1 :: Sequent -> [Form]
+ant1 (x, y) =  x
+
+-- zwraca listę poprzedników hipersekwentu
+f28 :: HyperSequent -> [[Form]]
+f28 xs = map ant1 xs
+
+
+test88 = f28 (head test2)
+test9  = [([C (I (N (V 1)) (D (V 2) (V 1))) (D (V 1) (V 2))], [D (V 1) (N (V 1)), I (V 10) (V 2), C (V 5) (V 6), C (V 5) (V 7), C (V 5) (V 22), C (V 5) (V 44)]), ([C (I (N (V 1)) (D (V 2) (V 1))) (D (V 1) (V 2))], [D (V 1) (N (V 1)), I (V 10) (V 2), C (V 5) (V 6), C (V 5) (V 7), C (V 5) (V 22), C (V 5) (V 44)])]
+
+
+-- to be continued
